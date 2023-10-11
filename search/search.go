f474bf231f4028a73a342e5557e5ed9615c5f054
Jay Reddy <jayreddy@gmail.com>
2023-10-10 23:36:53 -0700

    adds rotational bsearch

--- search/search.go
+++ search/search.go
@@ -23,3 +23,27 @@ func BSearch(arr []int, target int) int {
 	}
 	return -1
 }
+
+func RotatedBSearch(arr []int, target int) int {
+	// given a sorted arr rotated at random pivot, implement bsearch
+	lo, hi := 0, len(arr)
+	for lo < hi {
+		mid := (lo + hi) / 2
+		if target == arr[mid] {
+			return mid
+		} else if arr[lo] <= arr[mid] {
+			if arr[lo] <= target && target < arr[mid] {
+				hi = mid
+			} else {
+				lo = mid + 1
+			}
+		} else {
+			if arr[mid] < target && target <= arr[hi] {
+				lo = mid + 1
+			} else {
+				hi = mid
+			}
+		}
+	}
+	return -1 // sentinel value
+}
