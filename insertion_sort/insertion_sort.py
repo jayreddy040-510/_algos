22668fb3235ae6f6c9c7c6745bed83770e1fe043
Jay Reddy <jayreddy@gmail.com>
2023-12-25 11:36:22 -0800

    finishes insertion sort

---
+++ insertion_sort/insertion_sort.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+def insertion_sort(nums: List[int]) -> None:
+    for i in range(1, len(nums)):
+        for j in range(i, 0, -1):
+            if nums[j] < nums[j - 1]:
+                print(f"switching {nums[j]} and {nums[j - 1]}")
+                nums[j], nums[j - 1] = nums[j - 1], nums[j]
+            else:
+                break
+
+
+if __name__ == "__main__":
+    arr = [1, 5, 23, 4, 7, 2, 10]
+
+    insertion_sort(arr)
+
+    print(arr)
